2fb2ef02e17aa28291676fb0ad7f05b9118206da
Shane McGraw <shanemcgraw17@gmail.com>
2016-02-15 14:36:21 -0800

    Get user location data

--- forms/forms.js
+++ forms/forms.js
@@ -2,22 +2,34 @@ angular.module('wayfare.form', [])
 
 .controller('FormController', function ($scope, $http) {
   
-
-	if ("geolocation" in navigator) {
-	  /* geolocation is available */
-	  navigator.geolocation.getCurrentPosition(function(position) {
-	    do_something(position.coords.latitude, position.coords.longitude);
-	  });
-	} else {
-	  alert("Please ensure location services are enabled in your browser")
-	}
-
   $scope.data = {
+  	destinations: [
+	  	{
+	  		name: "Tartine Bakery",
+	  		latitude: 37.761418,
+	  		longitude: -122.424104
+	  	},
+  	],
   	location: {
-  		x: null,
-  		y: null
+  		latitude: null,
+  		longitude: null
   	}
   };
+
+	if ("geolocation" in navigator) {
+	  // geolocation is available
+	  navigator.geolocation.getCurrentPosition(function (position) {
+
+	    $scope.data.location.latitude = position.coords.latitude;
+	    $scope.data.location.longitude = position.coords.longitude;
+
+	  	console.log($scope.data);
+	  });
+
+	} else {
+	  alert("Please ensure location services are enabled in your browser");
+	}
+
 
   $scope.postLoc = function(loc){
     console.log("great scott! You entered:", loc);
