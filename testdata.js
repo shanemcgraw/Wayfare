c9b4d3d58bfe034a0ceb492156356c2a9f82017e
Shane McGraw <shanemcgraw17@gmail.com>
2016-02-15 16:17:51 -0800

    Create file for scratch data

---
+++ testdata.js
@@ -0,0 +1,34 @@
+// currency_code: "USD"
+// display_name: "uberX"
+// distance: 2.12
+// duration: 840
+// estimate: "$13-16"
+// high_estimate: 16
+// localized_display_name: "uberX"
+// low_estimate: 13
+// minimum: 8
+// product_id: "a1111c8c-c720-46c3-8534-2fcdd730040d"
+// surge_multiplier: 1.7
+
+
+
+//painted ladies
+//37.776103, -122.432738
+
+//coit tower
+//37.802174, -122.405865
+
+//fort point
+//37.810426, -122.476865
+
+//mission dolores
+//37.759467, -122.426398
+
+//ferry building
+//37.795333, -122.393494
+
+//ballpark
+//37.778256, -122.389238
+
+//fort mason
+//37.804933, -122.430329
