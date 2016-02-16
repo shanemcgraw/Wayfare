e3c8f4ecd5d212e5facf2ff6b871dc730d4fc060
Shane McGraw <shanemcgraw17@gmail.com>
2016-02-16 15:07:30 -0800

    Create for storing lat/long objects

---
+++ rideData.js
@@ -0,0 +1,9 @@
+var rideData = [
+  new google.maps.LatLng(37.752986, -122.403112),
+];
+
+// latitude: 37.761418
+// longitude: -122.424104
+// name: "Tartine Bakery"
+// priceEstimate: "$11-15 USD"
+// timeEstimate: "12 minutes"
