0a8ca3474f7962c17517a565708638017c71c2f6
Shane McGraw <shanemcgraw17@gmail.com>
2016-02-15 16:43:09 -0800

    Insert SF attraction data

--- forms/forms.js
+++ forms/forms.js
@@ -4,19 +4,51 @@ angular.module('wayfare.form', [])
   
 
   $scope.data = {
-  	destinations: [
-	  	{
-	  		name: "Tartine Bakery",
-	  		latitude: 37.761418,
-	  		longitude: -122.424104
-	  	},
-  	],
+  	destinations: [{
+			name: "Tartine Bakery",
+			latitude: 37.761418,
+			longitude: -122.424104
+		},
+		{
+			name: "The Painted Ladies",
+			latitude: 37.776103,
+			longitude: -122.432738
+		},
+		{
+			name: "Coit Tower",
+			latitude: 37.802174,
+			longitude: -122.405865
+		},
+		{
+			name: "Golden Gate Bridge",
+			latitude: 37.810426,
+			longitude: -122.476865
+		},
+		{
+			name: "Dolores Park",
+			latitude: 37.759467,
+			longitude: -122.426398
+		},
+		{
+			name: "Ferry Building",
+			latitude: 37.795333,
+			longitude: -122.393494
+		},
+		{
+			name: "AT&T Park",
+			latitude: 37.778256,
+			longitude: -122.389238
+		},
+		{
+			name: "Fort Mason",
+			latitude: 37.804933
+			longitude: -122.430329
+		}],
   	location: {
   		latitude: null,
   		longitude: null
   	}
   };
-
 
   var test = $scope.data.destinations[0];
   var currentLat;
@@ -77,13 +109,4 @@ angular.module('wayfare.form', [])
 	}
 
 
-
-
-
-	//Put all this on the server end one day
-
- 
-
-
-  //window.loc = $scope.loc;
 });
